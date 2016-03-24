a575d75fc025862e626af369387aac795d8711af
Phattharaphon Sriwichai <wai.yyyyy20@gmail.com>
2016-03-25 00:03:57 +0700

    first commit

---
+++ game01/core/src/main/java/sut/game01/core/HomeScreen.java
@@ -0,0 +1,56 @@
+package sut.game01.core;
+
+import static playn.core.PlayN.*;
+
+import react.UnitSlot;
+
+import playn.core.*;
+import playn.core.Image;
+import playn.core.ImageLayer;
+import tripleplay.game.*;
+
+public class HomeScreen extends UIScreen {
+
+  private final TestScreen testScreen;
+  private final ScreenStack ss;
+  
+  private Image bgImage;
+  private ImageLayer bgLayer;
+  private Image startButton;
+  private ImageLayer startLayer;
+
+  
+
+  public HomeScreen(final ScreenStack ss){
+    this.ss = ss;
+    testScreen = new TestScreen(ss);
+
+    bgImage = assets().getImage("images/homeBackground2.png");
+    bgLayer = graphics().createImageLayer(bgImage);
+    //graphics().rootLayer().add(bgLayer);
+
+    startButton = assets().getImage("images/startButton.png");
+    startLayer = graphics().createImageLayer(startButton);
+    //graphics().rootLayer().add(startLayer);
+    startLayer.setTranslation(220,140);
+
+
+      startLayer.addListener(new Mouse.LayerAdapter(){
+      @Override
+      public void onMouseUp(Mouse.ButtonEvent event){
+        ss.push(testScreen);
+      }
+    });
+      
+  }
+
+  @Override
+  public void wasShown() {
+      super.wasShown();
+
+      this.layer.add(bgLayer);
+      this.layer.add(startLayer);
+
+      
+  }
+}
---
+++ game01/core/src/main/java/sut/game01/core/TestScreen.java
@@ -0,0 +1,51 @@
+package sut.game01.core;
+
+import static playn.core.PlayN.*;
+
+import playn.core.*;
+import playn.core.Image;
+import playn.core.ImageLayer;
+import tripleplay.game.*;
+
+public class TestScreen extends UIScreen {
+  private final ScreenStack ss;
+  private Image bgImage;
+  private ImageLayer bgLayer;
+  private Image backButton;
+  private ImageLayer backLayer;
+
+ 
+
+  public TestScreen(final ScreenStack ss){
+    this.ss = ss;
+
+    bgImage = assets().getImage("images/bg.png");
+    bgLayer = graphics().createImageLayer(bgImage);
+    //graphics().rootLayer().add(bgLayer);
+
+    backButton = assets().getImage("images/backButton.png");
+    backLayer = graphics().createImageLayer(backButton);
+    //graphics().rootLayer().add(backLayer);
+    backLayer.setTranslation(10,10);
+
+    backLayer.addListener(new Mouse.LayerAdapter(){
+      @Override
+      public void onMouseUp(Mouse.ButtonEvent event){
+        ss.remove(ss.top());
+      }
+    });
+
+  
+  }
+
+  @Override
+  public void wasShown() {
+      super.wasShown();
+      this.layer.add(bgLayer);
+      this.layer.add(backLayer);
+
+
+  }
+
+  
+}
