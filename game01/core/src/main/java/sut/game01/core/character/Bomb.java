0461f3d10a450079a5ca0ec98dc0c09ca382a33f
Phattharaphon Sriwichai <wai.yyyyy20@gmail.com>
2016-05-12 01:49:20 +0700

    add n=bomb

---
+++ game01/core/src/main/java/sut/game01/core/character/Bomb.java
@@ -0,0 +1,134 @@
+package sut.game01.core.character;
+
+
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.*;
+import org.jbox2d.dynamics.contacts.Contact;
+import playn.core.Layer;
+import playn.core.util.Callback;
+import playn.core.util.Clock;
+import sut.game01.core.TestScreen;
+import sut.game01.core.sprite.Sprite;
+import sut.game01.core.sprite.SpriteLoader;
+
+public class Bomb {
+    private Sprite sprite;
+    private int spriteIndex = 0;
+    private boolean hasLoaded = false;
+    private float x;
+    private float y;
+    private Body body;
+    private boolean contacted;
+    private int contactCheck;
+    private Body other;
+
+
+    public enum State{
+        IDLE, BOOM
+    };
+    private State state = State.IDLE;
+    private int offset = 0;
+    private int e = 0;
+
+    public Bomb(final World world, final float x_px, final float y_px){
+        this.x = x_px;
+        this.y = y_px;
+
+        sprite = SpriteLoader.getSprite("images/sprites/bomb.json");
+        sprite.addCallback(new Callback<Sprite>(){
+            @Override
+            public void onSuccess(Sprite result){
+                sprite.setSprite(spriteIndex);
+                sprite.layer().setOrigin(sprite.width() / 2f,
+                        sprite.height() / 2f);
+                sprite.layer().setTranslation(x, y + 13f);
+                body = initPhysicsBody(world, TestScreen.M_PER_PIXEL * x_px,
+                        TestScreen.M_PER_PIXEL * y_px);
+
+                hasLoaded = true;
+            }
+
+            @Override
+            public void onFailure(Throwable cause){
+                //PlayN.log().error("Error loading image!", cause);
+            }
+
+        });
+    }
+
+    public Layer layer(){
+        return sprite.layer();
+    }
+    public void update(int delta) {
+        if (hasLoaded == false) return;
+        e += delta;
+        if (e > 150) {
+            switch (state) {
+                case IDLE: offset = 0;
+                    break;
+                case BOOM: offset = 6;
+                    break;
+            }
+            spriteIndex = offset + ((spriteIndex + 1) % 6);
+            sprite.setSprite(spriteIndex);
+            sprite.layer().setTranslation(body.getPosition().x / TestScreen.M_PER_PIXEL,
+                    body.getPosition().y / TestScreen.M_PER_PIXEL);
+            e = 0;
+        }
+    }
+
+    private Body initPhysicsBody(World world, float x, float y){
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.STATIC;
+        bodyDef.position = new Vec2(x, y);
+        Body body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(50 * TestScreen.M_PER_PIXEL/2,
+                50*TestScreen.M_PER_PIXEL / 2);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        //fixtureDef.density = 0.4f;
+        //fixtureDef.friction = 0.1f;
+        //fixtureDef.restitution = 0.35f;
+        body.createFixture(fixtureDef);
+
+        //body.createFixture(fixtureDef);
+
+        body.setLinearDamping(0.2f);
+        //body.setTransform(new Vec2(x, y), 0f);
+
+        return body;
+    }
+
+    public void paint(Clock clock){
+        if(!hasLoaded) return;
+
+        //sprite.layer().setRotation(body.getAngle());
+
+        sprite.layer().setTranslation(
+                (body.getPosition().x / TestScreen.M_PER_PIXEL),
+                body.getPosition().y / TestScreen.M_PER_PIXEL);
+
+    }
+
+    public Body getBody(){
+        return this.body;
+    }
+    public void contact(Contact contact){
+        contacted = true;
+        contactCheck = 0;
+
+        if(state == State.IDLE){
+            state = State.BOOM;
+            body.setActive(false);
+        }
+        if(contact.getFixtureA().getBody()==body){
+            other = contact.getFixtureB().getBody();
+        }else{
+            other = contact.getFixtureA().getBody();
+        }
+    }
+
+}
